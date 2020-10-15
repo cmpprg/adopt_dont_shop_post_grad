483dc1ab98e4efe1dcd9e5e1b39c34fe624d5826
Ryan Camp <campryan@comcast.net>
2020-10-15 16:26:21 -0600

    Create route and controller with index action for pet

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
 
+  #pets stuff
+  get '/pets', to: 'pets#index'
 end
