18c16d982ad79034ac496c0624e69bd47969117c
Ryan Camp <campryan@comcast.net>
2020-10-15 16:24:03 -0600

    Reconstruct factories for testing=

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,21 @@
 require 'faker'
 
 FactoryBot.define do
+
   factory :shelter do
     name { Faker::Company.unique.name }
     address { Faker::Address.unique.street_address }
     city { Faker::Address.unique.city }
     state { Faker::Address.unique.state_abbr }
     zip { Faker::Address.unique.zip_code }
+  end
+
+  factory :pet do
+    image { Faker::Company.unique.name }
+    name { Faker::Address.unique.street_address }
+    age { Faker::Address.unique.city }
+    sex { Faker::Address.unique.state_abbr }
+
+    shelter
   end
 end
