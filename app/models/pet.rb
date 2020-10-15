0a192d37c7ee1c324e04a25fbd65a79dbb76232a
Ryan Camp <campryan@comcast.net>
2020-10-15 16:16:06 -0600

    set up validations for pet and shelter models

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
-  
+  validates_presence_of :name
+  validates_presence_of :image
+  validates_presence_of :age
+  validates_presence_of :sex
+
+  belongs_to :shelter
 end
