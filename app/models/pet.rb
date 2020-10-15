0fcdbc451cbb1a9b1f7e0d4a0c7d218bd3f4f03e
Ryan Camp <campryan@comcast.net>
2020-10-15 17:42:04 -0600

    Create and run migration to add adoption status attr to pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,19 @@
 class Pet < ApplicationRecord
-  validates_presence_of :name
-  validates_presence_of :image
-  validates_presence_of :age
-  validates_presence_of :sex
+  validates :name,
+    presence: true
+
+  validates :image,
+    presence: true
+
+  validates :age,
+    presence: true
+
+  validates :sex,
+    presence: true
+
+  validates :adoption_status,
+    presence: true,
+    inclusion: { in: ['adoptable', 'pending'] }
 
   belongs_to :shelter
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_221253) do
+ActiveRecord::Schema.define(version: 2020_10_15_233139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_221253) do
     t.integer "age"
     t.string "sex"
     t.bigint "shelter_id"
+    t.string "adoption_status", default: "adoptable"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
