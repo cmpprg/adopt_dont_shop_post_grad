74a3466757b1ccbb4c38eafbb55e617256a10315
Ryan Camp <campryan@comcast.net>
2020-10-14 18:49:49 -0600

    Create route and controller action for new shelter

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   #shelters resources hand rolled
 
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
 end
