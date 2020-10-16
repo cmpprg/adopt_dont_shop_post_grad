745f84b8ed9190267748a8bc37c8e927d30e6520
Ryan Camp <campryan@comcast.net>
2020-10-16 09:44:59 -0600

    Write test to check delete pet functionality

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -46,4 +46,24 @@ RSpec.describe 'As a user on the pet show page' do
     expect(page).to have_current_path("/pets/#{pet.id}/edit")
   end
 
+  it "I can click link 'Delete Pet' and expect it to delete the resource from the app" do
+    pet_1 = create(:pet)
+    pet_2 = create(:pet)
+    pet_3 = create(:pet)
+
+    visit "/pets/#{pet_2.id}"
+
+    click_link('Delete Pet')
+
+    expect(page).to have_current_path('/pets')
+    expect(Pet.exists?(pet_2.id)).to eql(false)
+
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.description)
+    expect(page).to have_no_content(pet_2.name)
+    expect(page).to have_no_content(pet_2.description)
+    expect(page).to have_content(pet_3.name)
+    expect(page).to have_content(pet_3.description)
+  end
+
 end
